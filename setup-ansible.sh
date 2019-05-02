5040471b753bef51a94b9dada8aecebb9b20ab79
nycterent <saint@ghost.lt>
2019-05-02 19:25:25 +0000

    installs required software

---
+++ setup-ansible.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+yum install epel-release -y --quiet
+yum install -y ansible --quiet
+yum install git-core -y --quiet
+
+echo "`hostname -s` ansible_connection=local" > /tmp/ansible_host_sr
+
+ansible-playbook -i /tmp/ansible_host_sr playbooks/software.yml 
+
+
