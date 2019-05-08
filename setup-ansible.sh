a05df8229c69292a5808b5c92940feb526f49b0a
nycterent <saint@ghost.lt>
2019-05-08 19:30:08 +0000

    firewall stuff

--- setup-ansible.sh
+++ setup-ansible.sh
@@ -29,6 +29,7 @@ ansible-playbook -i /opt/tools/_etc/ansible_host_sr playbooks/httpd-config.yml
 ansible-playbook -i /opt/tools/_etc/ansible_host_sr playbooks/mysql-config.yml
 ansible-playbook -i /opt/tools/_etc/ansible_host_sr playbooks/sr-config.yml
 ansible-playbook -i /opt/tools/_etc/ansible_host_sr playbooks/selinux.yml
+ansible-playbook -i /opt/tools/_etc/ansible_host_sr playbooks/firewalld.yml
 ansible-playbook -i /opt/tools/_etc/ansible_host_sr playbooks/services.yml 
 
 echo "go to http://${hostname}/site or http://${ip}/site and login with: "
