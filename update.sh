6935e7687c3a6f2c9ce822679528f42d426b6aad
Mindaugas Žvirblis <tinklapiai@gmail.com>
2019-05-21 10:32:16 +0300

    change exit code and typo

--- update.sh
+++ update.sh
@@ -2,8 +2,8 @@
 
 git pull
 
-ansible-playbook -i /opt/tools/_etc/ansible_host_sr playbooks/files.yml
-ansible-playbook -i /opt/tools/_etc/ansible_host_sr playbooks/supervisord.yml
-ansible-playbook -i /opt/tools/_etc/ansible_host_sr playbooks/mysql-migrations.yml
+ansible-playbook -i /opt/tools/_etc/ansible_host_sr playbooks/files.yml || true
+ansible-playbook -i /opt/tools/_etc/ansible_host_sr playbooks/supervisord.yml || true
+ansible-playbook -i /opt/tools/_etc/ansible_host_sr playbooks/mysql-migrations.yml || true
 
-supervisortctl update
+supervisorctl update
