16f29de83e902af877fc834c10d978ec2e854474
Mindaugas Žvirblis <tinklapiai@gmail.com>
2019-05-21 10:27:30 +0300

    update supervisor after update

--- update.sh
+++ update.sh
@@ -5,3 +5,5 @@ git pull
 ansible-playbook -i /opt/tools/_etc/ansible_host_sr playbooks/files.yml
 ansible-playbook -i /opt/tools/_etc/ansible_host_sr playbooks/supervisord.yml
 ansible-playbook -i /opt/tools/_etc/ansible_host_sr playbooks/mysql-migrations.yml
+
+supervisortctl update
