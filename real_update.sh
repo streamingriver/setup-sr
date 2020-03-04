af421adb9f73052ab66bbbade5c6841b6ee2956f
Mindaugas Žvirblis <mz@9g.lt>
2020-03-04 10:40:46 +0200

    update.sh

--- real_update.sh
+++ real_update.sh
@@ -4,6 +4,7 @@
 ansible-playbook -i /opt/tools/_etc/ansible_host_sr playbooks/files.yml || true
 ansible-playbook -i /opt/tools/_etc/ansible_host_sr playbooks/supervisord.yml || true
 ansible-playbook -i /opt/tools/_etc/ansible_host_sr playbooks/mysql-migrations.yml || true
+ansible-playbook -i /opt/tools/_etc/ansible_host_sr playbooks/software.yml || true
 
 restorecon -R /var/www/sr # 
 
